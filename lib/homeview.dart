f93b571bbdcfeb60ee7fb62c939c76f3a9064e0d
Ethel Danquah <etheldanquah@gmail.com>
2021-12-12 08:00:52 -0600

    commit on main

--- lib/homeview.dart
+++ lib/homeview.dart
@@ -108,7 +108,7 @@ class HomeView extends StatelessWidget {
         padding: EdgeInsets.all(10.0),
         child: Center(
           child: CircleAvatar(radius: 30,
-          backgroundImage: AssetImage('assets/avatar.JPG'),),
+          backgroundImage: NetworkImage('https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'),),
         ),
       )
     ],
